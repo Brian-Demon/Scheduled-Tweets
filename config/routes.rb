80e5767e87dd7b1ab1e7527a8ea023075586dd00
Brian-Demon <BrianWMatejka@gmail.com>
2021-02-25 15:07:06 -0600

    C.R.U.D Tweets

--- config/routes.rb
+++ config/routes.rb
@@ -22,8 +22,7 @@ Rails.application.routes.draw do
   get "/auth/twitter/callback", to: "omniauth_callbacks#twitter"
 
   resources :twitter_accounts
-  # get "twitter_accounts/:id"
-  # delete "twitter_accounts/:id"
+  resources :tweets
 
   root to: "main#index"
 
